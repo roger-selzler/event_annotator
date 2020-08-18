80a986dc196f2316530f5b3845da39ff347d277a
roger-selzler <32464831+roger-selzler@users.noreply.github.com>
2020-08-18 08:44:17 -0400

    Update example.py

--- example.py
+++ example.py
@@ -8,17 +8,17 @@ import event_annotator
 ea = event_annotator.Event_annotator()
 
 # #load sample data
-# ecg_data = event_annotator.load_csv_data('ecg')
-# accel_data = event_annotator.load_csv_data('accel')
+ecg_data = event_annotator.load_csv_data('ecg')
+accel_data = event_annotator.load_csv_data('accel')
 
 # # add signals to ea through code
-# ea.add_signal(x=ecg_data.x,y=ecg_data.y,yf=ecg_data.yf,name='Ecg')
-# ea.add_signal(x=accel_data.x,y=accel_data.y,yf=accel_data.yf,name='Accelerometer')
+ea.add_signal(x=ecg_data.x,y=ecg_data.y,yf=ecg_data.yf,name='Ecg')
+ea.add_signal(x=accel_data.x,y=accel_data.y,yf=accel_data.yf,name='Accelerometer')
 
 
 # # add labels to ea through code
-# ea.add_label('RComplex',label_type='points',filename_loc = 'RComplex')
-# ea.add_label('Movement',label_type='range',filename_loc = 'Movement')
+ea.add_label('RComplex',label_type='points',filename_loc = 'RComplex')
+ea.add_label('Movement',label_type='range',filename_loc = 'Movement')
 
 # run the application
 ea.run()
